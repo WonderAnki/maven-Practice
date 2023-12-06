c7fef7e821eeb4cca423b75b9305ef212930bb8f
Ankita Seth <ankitaseth066@gmail.com>
2023-12-06 19:01:50 +0530

    myfirst commit

---
+++ DemoMavenProject/src/main/java/UpgradePractice/App.java
@@ -0,0 +1,13 @@
+package UpgradePractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
